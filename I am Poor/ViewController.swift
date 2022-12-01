bc09e852acab8dc95b93c2f7a42b169a3dff124b
Raíssa Queiroz <raissa.queiroz@dtidigital.com.br>
2022-12-01 14:45:54 -0300

    initial commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Raíssa Queiroz on 01/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
